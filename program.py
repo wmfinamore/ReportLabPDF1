f76866cba51093a084132e56228084813272e6da
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-04-11 22:31:40 -0300

    Implemented base layout for our report with header, body and footer

--- program.py
+++ program.py
@@ -6,10 +6,21 @@ from reportlab.platypus import Table
 pdf = canvas.Canvas('report.pdf', pagesize = A4)
 pdf.setTitle('Palms Hotel')
 
+width, height = A4
+
+heightList = [
+    height * 0.20,
+    height * 0.77,
+    height * 0.03,
+]
+
 mainTable = Table([
-    ['texto simples', 'nova coluna'],
-    ['nova linha', 12546513246513]
-])
+    ['header'],
+    ['body'],
+    ['footer']
+    ],
+        colWidths = width,
+        rowHeights = heightList,)
 
 mainTable.setStyle([
     ('GRID', (0, 0), (-1, -1), 1, 'red'),
