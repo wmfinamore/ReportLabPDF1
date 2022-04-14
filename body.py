a5b91a7176cc20c5bece02d594206eeda31be347
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-04-14 17:27:33 -0300

    Defined functions to generate internal content for the report

--- body.py
+++ body.py
@@ -3,26 +3,26 @@ from reportlab.platypus import Table
 def genBodyTable(width, height):
     
     widthList = [
-        width * 0.10,
-        width * 0.80,
-        width * 0.10,
+        width * 0.10, #left 'padding'
+        width * 0.80, # values
+        width * 0.10, # right 'padding'
     ]
     
     heightList = [
-        height * 0.10,
-        height * 0.15,
-        height * 0.35,
-        height * 0.30,
-        height * 0.10,
+        height * 0.10, # offer 0
+        height * 0.15, # contacts 1
+        height * 0.35, # prices 2
+        height * 0.30, # description 3
+        height * 0.10, # about 4
         
     ]
     
     res = Table([
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', ''],
-        ['', '', ''],
+        ['', 'Offer', ''],
+        ['', _genContactsTable(widthList[1], heightList[1]), ''],
+        ['', _genPriceListTable(widthList[1], heightList[2]), ''],
+        ['', _genDescriptionParasList(), ''],
+        ['', _genAboutTable(widthList[1], heightList[4]), ''],
     ],
         widthList,
         heightList)
@@ -32,3 +32,17 @@ def genBodyTable(width, height):
     ])
     
     return res
+
+## Functions for internal tables
+
+def _genContactsTable(width, height):
+    return 'CONTACTS'
+
+def _genPriceListTable(width, height):
+    return 'PRICES'
+
+def _genDescriptionParasList():
+    return 'DESCRIPTION'
+
+def _genAboutTable(width, height):
+    return 'ABOUT'
