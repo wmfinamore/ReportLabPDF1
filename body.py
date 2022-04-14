be6880f764a1d7d44243b0997014b70230c2fc38
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-04-14 17:13:04 -0300

    Implemented basic layout for body of the report

--- body.py
+++ body.py
@@ -1,3 +1,34 @@
+from reportlab.platypus import Table
 
 def genBodyTable(width, height):
-    return "BODY"
+    
+    widthList = [
+        width * 0.10,
+        width * 0.80,
+        width * 0.10,
+    ]
+    
+    heightList = [
+        height * 0.10,
+        height * 0.15,
+        height * 0.35,
+        height * 0.30,
+        height * 0.10,
+        
+    ]
+    
+    res = Table([
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+        ['', '', ''],
+    ],
+        widthList,
+        heightList)
+    
+    res.setStyle([
+        ('GRID', (0, 0), (-1, -1), 1, 'red'),
+    ])
+    
+    return res
