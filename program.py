81a911b9fcd043454ad923220f99f20b68dcb2df
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-04-23 21:22:55 -0300

    Implemented function to enable us to create many pages with our report

--- program.py
+++ program.py
@@ -38,46 +38,56 @@ pdfmetrics.registerFontFamily(
 )
 ##########################################################
 
+def genPalmsHotelPage(pdf, size):
+    # setting width and height page
+    width, height = size
+
+    # heights list
+    heightList = [
+        height * 0.20,
+        height * 0.77,
+        height * 0.03,
+    ]
+
+    # create a table
+    mainTable = Table([
+        [genHeaderTable(width, heightList[0])],
+        [genBodyTable(width, heightList[1])],
+        [genFooterTable(width, heightList[2])]
+        ],
+            colWidths = width,
+            
+            # apply heights list
+            rowHeights = heightList,)
+
+    # defining style format for the table
+    mainTable.setStyle([
+        # ('GRID', (0, 0), (-1, -1), 1, 'red'),
+        
+        ('LEFTPADDING', (0, 0), (0, 2), 0),
+        
+        ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
+        
+    ])
+
+    # wrap the canvas with the table
+    mainTable.wrapOn(pdf, 0, 0)
+    mainTable.drawOn(pdf, 0, 0)
+#END genPalmsHotelPage()
+
+size = A4
+
 #define canvas to draw the report
-pdf = canvas.Canvas('report.pdf', pagesize = A4)
+pdf = canvas.Canvas('report.pdf', pagesize = size)
 pdf.setTitle('Palms Hotel')
 
-# setting width and height page
-width, height = A4
+genPalmsHotelPage(pdf, size)
+pdf.showPage() # Page Break 
 
-# heights list
-heightList = [
-    height * 0.20,
-    height * 0.77,
-    height * 0.03,
-]
+genPalmsHotelPage(pdf, size)
+pdf.showPage()
 
-# create a table
-mainTable = Table([
-    [genHeaderTable(width, heightList[0])],
-    [genBodyTable(width, heightList[1])],
-    [genFooterTable(width, heightList[2])]
-    ],
-        colWidths = width,
-        
-        # apply heights list
-        rowHeights = heightList,)
-
-# defining style format for the table
-mainTable.setStyle([
-    # ('GRID', (0, 0), (-1, -1), 1, 'red'),
-    
-    ('LEFTPADDING', (0, 0), (0, 2), 0),
-    
-    ('BOTTOMPADDING', (0, 0), (-1, -1), 0),
-    
-])
-
-# wrap the canvas with the table
-mainTable.wrapOn(pdf, 0, 0)
-mainTable.drawOn(pdf, 0, 0)
-
-# apply configurations in our canvas
+genPalmsHotelPage(pdf, size)
 pdf.showPage()
 
 # generate pdf file
