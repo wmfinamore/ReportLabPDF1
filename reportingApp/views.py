9520afef21ef233f4f3b53d91b57e56abb0562e3
Willian Moreira Finamore <wmfinamore@hotmail.com>
2022-04-27 09:04:42 -0300

    Implemented function to receive POST data

--- reportingApp/views.py
+++ reportingApp/views.py
@@ -3,3 +3,14 @@ from django.shortcuts import render
 
 def index(request):
     return render(request, 'reportingApp/index.html')
+
+def genPDF(request):
+    
+    values = request.POST
+    
+    filename = values.get('fname', 'example.pdf')
+    pagesize = values.get('pagesize', 'A4')
+    orientation = values.get('orientation', 'landscape')
+    userpass = values.get('userpass', '')
+    onwerpass = values.get('ownerpass', '')
+    return render(request, 'reportingApp/index.html')
